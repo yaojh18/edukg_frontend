328f5be941a0ea60eddf4f92bf635fc30c77c9bd
hanyullai <hhyy@git>
2021-08-16 16:01:43 +0800

    doing main page

--- edu_app/app/src/main/java/com/example/edu_app/MainActivity.java
+++ edu_app/app/src/main/java/com/example/edu_app/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.edu_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.ViewPager;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
@@ -13,17 +19,43 @@ import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.IOException;
 
+import com.example.edu_app.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private MenuItem menuItem;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        request();
+
     }
+
+    private void mainPageInit() {
+        ActivityMainBinding binding;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        BottomNavigationView navView = findViewById(R.id.bottom_navigation);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
+    }
+
 
     private void request() {
         String username = "hanyu";
@@ -50,6 +82,10 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("test", (response.body().string()));
             }
         });
+    }
+
+    public void search(View view) {
+        Log.e("test", "search");
     }
 }
 
